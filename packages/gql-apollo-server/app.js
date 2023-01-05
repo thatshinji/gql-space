1deb462da63668d72133be89aee753ff2c14c1fe
thatshinji <li2.y@foxmail.com>
2023-01-05 22:21:02 +0800

    feat: add koa middleware

--- packages/gql-apollo-server/app.js
+++ packages/gql-apollo-server/app.js
@@ -1,5 +1,10 @@
+import http from "http";
+import Koa from "koa";
+import bodyParser from "koa-bodyparser";
+import cors from "@koa/cors";
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { koaMiddleware } from "@as-integrations/koa";
 
 const typeDefs = `#graphql
   type Book {
@@ -22,21 +27,32 @@ const books = [
     author: "Paul Auster",
   },
 ];
-
-// Resolver
+// A map of functions which return data for the schema.
 const resolvers = {
   Query: {
     books: () => books,
   },
 };
 
+const app = new Koa();
+const httpServer = http.createServer(app.callback());
+
+// Set up Apollo Server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
-});
+await server.start();
 
-console.log(`🚀  Server ready at: ${url}`);
+app.use(cors());
+app.use(bodyParser());
+app.use(
+  koaMiddleware(server, {
+    context: async ({ ctx }) => ({ token: ctx.headers.token }),
+  })
+);
+
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+console.log(`🚀 Server ready at http://localhost:4000`);
